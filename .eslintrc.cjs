15b8d88eb32afccd504b81be61cf2cead2dcf04f
VitaliySaburdo <vsaburdo@gmail.com>
2024-03-05 20:48:36 +0200

    fix linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "@wemake-services/javascript"
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    '@wemake-services/javascript'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
