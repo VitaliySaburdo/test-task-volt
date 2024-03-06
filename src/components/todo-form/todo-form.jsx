0ece7f8d717b700c28c3f6bcbe5210ffd96ef146
VitaliySaburdo <vsaburdo@gmail.com>
2024-03-06 11:16:24 +0200

    add form

--- src/components/todo-form/todo-form.jsx
+++ src/components/todo-form/todo-form.jsx
@@ -1,11 +1,25 @@
 // import { useState } from 'react'
+import {
+  Form,
+  Label,
+  Container,
+  Title,
+  Input,
+  Button,
+} from './todo-form.styled'
 export const ToDoForm = () => {
   // const [name, setName] = useState('')
   return (
-    <form>
-      <label>
-        <input/>
-      </label>
-    </form>
+    <>
+      <Container>
+        <Title>Create task</Title>
+        <Form>
+          <Label>
+            <Input />
+          </Label>
+          <Button>Add task</Button>
+        </Form>
+      </Container>
+    </>
   )
 }
