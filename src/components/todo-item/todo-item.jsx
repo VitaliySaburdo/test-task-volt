4a7842972ab3fc29a507a3945f6b7c91831bdda8
VitaliySaburdo <vsaburdo@gmail.com>
2024-03-08 09:54:47 +0200

    checkbox

--- src/components/todo-item/todo-item.jsx
+++ src/components/todo-item/todo-item.jsx
@@ -3,6 +3,7 @@ import { deleteTask, toggleCompleted } from '../../redux/operations'
 import { Item } from './todo-item.styled'
 import { MuiButton } from '../button/button'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Checkbox from '@mui/material/Checkbox'
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch()
@@ -13,10 +14,19 @@ export const Task = ({ task }) => {
 
   return (
     <Item>
-      <input type="checkbox" checked={task.completed} onChange={handleToggle} />
+      <Checkbox
+        checked={task.completed}
+        onChange={handleToggle}
+        sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }}
+      />
       <p>{task.text}</p>
-      <MuiButton color='error' startIcon={<DeleteIcon />} onClick={handleDelete}>Delete</MuiButton>
+      <MuiButton
+        color="error"
+        startIcon={<DeleteIcon />}
+        onClick={handleDelete}
+      >
+        Delete
+      </MuiButton>
     </Item>
   )
 }
- 
