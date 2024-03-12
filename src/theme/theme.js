11a923274165924d983d650ebbfc662cabd9cd29
VitaliySaburdo <vsaburdo@gmail.com>
2024-03-12 08:50:36 +0200

    remove themes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,36 +1,4 @@
 export const theme = Object.freeze({
-  'fonts': {
-    'main': '\'Montserrat\', sans-serif',
-    'second': '\'Open Sans\', sans-serif',
-    'accent': '\'Sail\', cursive',
-  },
-  'fontSizes': {
-    'mainFontSize': '14x',
-    'btnFontSize': '18px',
-    'titleSize': '50px',
-  },
-  'fontWeights': {
-    'regular': 400,
-    'medium': 500,
-    'semibold': 600,
-    'bold': 700,
-  },
-  'colors': {
-    // My styles
-    'textColor': '#444251',
-    'witeTextColor': '#FFFFFF',
-    'primaryColor': '#FFD4DD',
-    'secondaryColor': '#9C0746',
-    'btnTxtColor': '#FFFFFF',
-    'secondaryTextColor': '#b44e79',
-    'titleTextColor': '#a0114e',
-    'witeBcgColor': '#FFFFFF',
-    'blackBcgColor': '#000000',
-    'btnHoverColor': '#bf1c60',
-    'scrollBcgColor': '#555555',
-    'overlayColor': '#61616199',
-
-  },
   'media': {
     'sm': '(min-width: 360px)',
     'md': '(min-width: 768px)',
